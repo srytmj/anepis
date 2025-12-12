d254d15b7e47a05a13d0f67440a1a7bd228ea640
Maja <suryatmaja.dev@gmail.com>
2025-12-12 23:12:31 +0700

    Update 2025_12_01_030851_create_students_table.php

--- database/migrations/2025_12_01_030851_create_students_table.php
+++ database/migrations/2025_12_01_030851_create_students_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('student', function (Blueprint $table) {
             $table->id();
-            $table->integer('studentid')->unique();
+            $table->integer('studentid', 20)->unique();
             $table->string('email')->unique();
             $table->string('name');
             $table->string('transcript')->nullable();
